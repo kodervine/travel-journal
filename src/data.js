0777c6e265078fca6c27270bcea61d319325f05b
Laskellyb <anikwenzekelly@gmail.com>
2022-08-13 10:58:42 +0100

    Made my image sharable for tinapa and pleasure park

--- src/data.js
+++ src/data.js
@@ -6,8 +6,8 @@ export default [
         startDate: "25 July, 2016",
         endDate: "31 July, 2016",
         description: "Tinapa is a business and leisure resort located North of Calabar municipality, Cross River State, Nigeria associated with the Calabar Free Trade Zone",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
-        // imageUrl: "https://photos.app.goo.gl/se4ejLjEqtwAZqpq5"
+        // imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        imageUrl: "https://lh3.googleusercontent.com/pw/AL9nZEXb-oiBv-7QV1NBMkBZpv6irgRu7qajlIOK9YxdlbXq-gEv7FmEPNMRpGlc5bprMP1Z3Lt3PeDJf446r_7dPysJL1R-cGudPveC_UkZIjociZ6BEyAFogKNNYJekVCWtsmU917IevE4IZjz5I8_o_pgCg=w352-h625-no?authuser=0"
     },
     {
         title: "Portharcourt Pleasure park",
@@ -16,8 +16,8 @@ export default [
         startDate: "3 Dec, 2021",
         endDate: "3 Dec, 2021",
         description: "Port Harcourt PLEASURE PARK is a public recreation park in Niger-Delta's oil rich garden city of Port Harcourt, Rivers State, Nigeria. It is strategically located along the major Aba Road, and flanked between the Army Barracks and Air Force base junction,",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
-        // imageUrl: "https://photos.app.goo.gl/WqS9ZbBLxK8rgF9cA"
+        // imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        imageUrl: "https://lh3.googleusercontent.com/pw/AL9nZEXgWMU8PKuCl9kDajosNxm_577p3LFpTP49lh9kCuwynt4HR8jSr3MbHwRnysX6cTghNZMoHZZIWI1OFZtzhMAgHWvpFDe-y7o2HHmEEIJZQ_vNod1GV2j-OoZp1wJSQY857Hwf77RZy9r6gbgFvGd60Q=w469-h625-no?authuser=0"
     },
     {
         title: "Mount Fuji",
